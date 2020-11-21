d7344fa107fdcee87f14332f176c95c36cc3a533
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-21 20:40:35 +0900

    feat: add new project

---
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -0,0 +1,14 @@
+package jp.kaleidot725.sample
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import jp.kaleidot725.sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
