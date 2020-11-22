2b42db6701edccda0aa7cba39250a71865340366
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-11-22 18:12:24 +0900

    feat: add sub activity

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.kaleidot725.sample
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,15 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import jp.kaleidot725.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val navigateButton = findViewById<Button>(R.id.navigate_button)
+        navigateButton.setOnClickListener {
+            startActivity(Intent(this, SubActivity::class.java))
+        }
+    }
+}
